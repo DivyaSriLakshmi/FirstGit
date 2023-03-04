7f6c9efd86a2fbc43f24c03b2d9535821516a07e
DivyaSriLakshmi <divyasrisundaraneedi4@gmail.com>
2023-03-04 20:10:33 +0530

    Task5

--- dom.js
+++ dom.js
@@ -24,15 +24,40 @@
 //let tit=document.getElementsByClassName('title');
 //tit[0].style.fontWeight="bold";
 //tit[0].style.color='green';
-let items=document.getElementsByClassName('list-group-item');
+//let items=document.getElementsByClassName('list-group-item');
 //console.log(items);
 //console.log(items[1]);
-items[1].textContent='Hello 2';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold";
-}
+//items[1].textContent='Hello 2';
+//for(var i=0;i<items.length;i++){
+   // items[i].style.fontWeight="bold";
+//}
 //items[i].style.fontWeight='bold';
-items[2].style.backgroundColor='green';
+//items[2].style.backgroundColor='green';
+
+
+//GETELEMENTSBYTAGNAME
+//var li=document.getElementsByTagName('li');
+//li[1].textContent='Hello 2';
+//li[1].style.fontWeight='bold';
+//li[1].style.backgroundColor='yellow';
+//for(var i=0;i<li.length;i++){
+   // li[i].style.backgroundColor='#f4f4f4';
+//}
+//li.document.createElement(li);
+let list = document.getElementById('items');
+let li =document.createElement('li');
+li.className = 'list-group-item';
+//li.document.createTextNode='Item 5';
+li. appendChild (document.createTextNode('Item 5'))
+list.appendChild(li);
+//console.log('li');
+
+//QUERYSELECTOR
+
+//var header=document.querySelector('#main-header');
+//header.style.borderBottom='solid 4px #ccc';
+
+//var input
 
 
 
